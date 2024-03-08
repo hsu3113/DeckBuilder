0561c29b44799a30386b95e1d89bfe1c10cc65b4
sini3113 <hsu3113@naver.com>
2024-03-08 11:56:08 +0900

    드로우와 셔플 시스템 따라 만들기

---
+++ Assets/02.Scripts/GameManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<Card> deck = new List<Card>();
+    public List<Card> discardPile = new List<Card>();
+    public Transform[] cardSlots;
+    public bool[] availableCardSlots;
+    
+    public TMP_Text deckSizeText;
+    public TMP_Text discardPileText;
+    
+    public void DrawCard()
+    {
+        if (deck.Count >= 1)
+        {
+            Card randomCard =  deck[Random.Range(0,deck.Count)];
+            for (int i = 0; i < availableCardSlots.Length; i++)
+            {
+                if(availableCardSlots[i] == true)
+                {
+                    randomCard.gameObject.SetActive(true);
+                    randomCard.handIndex = i;
+                    randomCard.transform.position = cardSlots[i].position;
+                    randomCard.hasBeenPlayed = false;
+                    availableCardSlots[i] = false;
+                    deck.Remove(randomCard);
+                    return;
+                }
+            }
+        }
+    }
+
+    public void Shuffle()
+    {
+        if(discardPile.Count >= 1)
+        {
+            foreach(Card card in discardPile)
+            {
+                deck.Add(card);
+            }
+                discardPile.Clear();
+        }
+    }
+
+    private void Update()
+    {
+        deckSizeText.text = deck.Count.ToString();
+        discardPileText.text = discardPile.Count.ToString();
+    }
+}
