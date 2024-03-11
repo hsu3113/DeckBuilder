e2cd71221bf6966578ac26d47acb38576935d1c7
sini3113 <hsu3113@naver.com>
2024-03-11 23:02:48 +0900

    Effect, Wrtie 클래스 생성

--- Assets/02.Scripts/Card.cs
+++ Assets/02.Scripts/Card.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Card : MonoBehaviour
@@ -7,12 +10,16 @@ public class Card : MonoBehaviour
     public bool hasBeenPlayed;
 
     public int handIndex;
+    public string cardName,cardCost,cardInform;
+    public int[] figure;
 
     private GameManager gm;
+    public TMP_Text Name, Cost, Inform;
 
     private void Start()
     {
         gm = FindObjectOfType<GameManager>();
+
     }
 
     private void OnMouseDown()
@@ -29,5 +36,24 @@ public class Card : MonoBehaviour
     {
         gm.discardPile.Add(this);
         gameObject.SetActive(false);
+    }
+}
+
+class Effect
+{
+    Card card = new Card();
+    void Brave(int dmg)
+    {
+         card.cardInform += $"용기를 {dmg}얻는다. ";
+    }
+}
+class Write
+{
+    Card card = new Card();
+    void Writing(string n, string c, string i)
+    {
+        card.Name.text = n;
+        card.Cost.text = c;
+        card.Inform.text = i;
     }
 }
