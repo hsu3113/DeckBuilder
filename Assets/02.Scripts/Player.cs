b1ac769160170f73e6ec1d70a37318d462151ba3
sini3113 <hsu3113@naver.com>
2024-03-12 20:20:43 +0900

    divinePower; flame, thorn, weaponBreak, echo Effect도 제작중

--- Assets/02.Scripts/Player.cs
+++ Assets/02.Scripts/Player.cs
@@ -6,17 +6,54 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private int brave;
-    private bool shildBreak;
+    private bool shieldBreak;
+    private int divinePower;
+    private int flame;
+    private int thorn;
+    private bool weaponBreak;
+    private bool echo;
     public Transform[] effects;
     public Transform[] effectSlots;
     public bool[] availableEffectSlots;
 
 
 
-    public void GetBrave(int b){
+    public void GetBrave(int b)
+    {
         brave += b;
+        LoadEffects();
+    }
+    public void TakeShieldBreak()
+    {
+        shieldBreak = true;
+        LoadEffects();
     }
 
+    public void GetDivinePower(int d)
+    {
+        divinePower += d;
+        LoadEffects();
+    }
+    public void GetFlame(int f)
+    {
+        flame += f;
+        LoadEffects();
+    }
+    public void GetThorn(int t)
+    {
+        thorn += t;
+        LoadEffects();
+    }
+    public void TakeWeaponBreak()
+    {
+        weaponBreak = true;
+        LoadEffects();
+    }
+    public void GetEcho()
+    {
+        echo = true;
+        LoadEffects();
+    }
     public void LoadEffects()
     {
         if(brave != 0)
@@ -34,8 +71,8 @@ public class Player : MonoBehaviour
         }
         else effects[0].gameObject.SetActive(false);
 
-        if (shildBreak)
-        {
+        if (shieldBreak)
+        {   
             effects[1].gameObject.SetActive(true);
             for (int i = 0; i < availableEffectSlots.Length; i++)
             {
@@ -47,5 +84,78 @@ public class Player : MonoBehaviour
             }
         }
         else effects[1].gameObject.SetActive(false);
+
+        if(divinePower != 0)
+        {
+            effects[2].gameObject.SetActive(true);
+            effects[2].GetComponentInChildren<TMP_Text>().text = brave.ToString();
+            for (int i = 0; i < availableEffectSlots.Length; i++)
+            {
+                if(availableEffectSlots[i])
+                {
+                    transform.position = effectSlots[i].position;
+                    availableEffectSlots[i] = false;
+                }
+            }
+        }
+        else effects[2].gameObject.SetActive(false);
+
+         if(flame != 0)
+        {
+            effects[3].gameObject.SetActive(true);
+            effects[3].GetComponentInChildren<TMP_Text>().text = brave.ToString();
+            for (int i = 0; i < availableEffectSlots.Length; i++)
+            {
+                if(availableEffectSlots[i])
+                {
+                    transform.position = effectSlots[i].position;
+                    availableEffectSlots[i] = false;
+                }
+            }
+        }
+        else effects[3].gameObject.SetActive(false);
+
+         if(thorn != 0)
+        {
+            effects[4].gameObject.SetActive(true);
+            effects[4].GetComponentInChildren<TMP_Text>().text = brave.ToString();
+            for (int i = 0; i < availableEffectSlots.Length; i++)
+            {
+                if(availableEffectSlots[i])
+                {
+                    transform.position = effectSlots[i].position;
+                    availableEffectSlots[i] = false;
+                }
+            }
+        }
+        else effects[4].gameObject.SetActive(false);
+
+        if (weaponBreak)
+        {   
+            effects[5].gameObject.SetActive(true);
+            for (int i = 0; i < availableEffectSlots.Length; i++)
+            {
+                if(availableEffectSlots[i])
+                {
+                    transform.position = effectSlots[i].position;
+                    availableEffectSlots[i] = false;
+                }
+            }
+        }
+        else effects[5].gameObject.SetActive(false);
+
+        if (echo)
+        {   
+            effects[6].gameObject.SetActive(true);
+            for (int i = 0; i < availableEffectSlots.Length; i++)
+            {
+                if(availableEffectSlots[i])
+                {
+                    transform.position = effectSlots[i].position;
+                    availableEffectSlots[i] = false;
+                }
+            }
+        }
+        else effects[6].gameObject.SetActive(false);
     }
 }
